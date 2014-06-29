27c1926db3b75fd8d672d9bdd3dd9b6bb6f2eac1
Schmoller <steven.schmoll@gmail.com>
2014-06-29 23:14:06 +1000

    Added log forwarding for BungeeCord

---
+++ Bungee/src/main/java/au/com/addstar/rcon/RemoteConsoleLogHandler.java
@@ -0,0 +1,40 @@
+package au.com.addstar.rcon;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+import au.com.addstar.rcon.network.packets.main.PacketOutMessage.MessageType;
+import au.com.addstar.rcon.server.RconServer;
+import au.com.addstar.rcon.server.ServerNetworkManager;
+
+public class RemoteConsoleLogHandler extends Handler
+{
+	@Override
+	public void publish( LogRecord record )
+	{
+		MessageType type = MessageType.Log;
+		if(record.getThrown() != null)
+			type = MessageType.Exception;
+		
+		String message = getFormatter().format(record);
+		if(message.endsWith("\n"))
+			message = message.substring(0, message.length()-1);
+		
+		for(ServerNetworkManager connection : RconServer.instance.getConnections())
+		{
+			BungeeUser user = (BungeeUser)connection.getUser();
+			((UserCommandSender)user.asCommandSender()).sendMessage(message, type);
+		}
+	}
+
+	@Override
+	public void flush()
+	{
+	}
+
+	@Override
+	public void close() throws SecurityException
+	{
+	}
+
+}
--- Bungee/src/main/java/au/com/addstar/rcon/RemoteConsolePlugin.java
+++ Bungee/src/main/java/au/com/addstar/rcon/RemoteConsolePlugin.java
@@ -2,12 +2,17 @@ package au.com.addstar.rcon;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
 
 import au.com.addstar.rcon.network.HandlerCreator;
 import au.com.addstar.rcon.network.NetworkManager;
 import au.com.addstar.rcon.network.handlers.INetworkHandler;
 import au.com.addstar.rcon.server.RconServer;
 import au.com.addstar.rcon.server.ServerLoginHandler;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class RemoteConsolePlugin extends Plugin
@@ -15,6 +20,7 @@ public class RemoteConsolePlugin extends Plugin
 	public static RemoteConsolePlugin instance;
 	
 	private RconServer mServer;
+	private RemoteConsoleLogHandler mLogHandler;
 	
 	@Override
 	public void onEnable()
@@ -50,11 +56,37 @@ public class RemoteConsolePlugin extends Plugin
 				return new NetHandler(manager);
 			}
 		});
+		
+		installLogHandler();
 	}
 	
 	@Override
 	public void onDisable()
 	{
 		mServer.shutdown();
+		
+		Logger bungeeCordLog = ProxyServer.getInstance().getLogger();
+		bungeeCordLog.removeHandler(mLogHandler);
 	}
+	
+	private void installLogHandler()
+	{
+		Logger bungeeCordLog = ProxyServer.getInstance().getLogger();
+		
+		Formatter format = null;
+		
+		for(Handler handler : bungeeCordLog.getHandlers())
+		{
+			if(handler instanceof FileHandler)
+				format = handler.getFormatter();
+			
+			if(format == null)
+				break;
+		}
+		
+		mLogHandler = new RemoteConsoleLogHandler();
+		mLogHandler.setFormatter(format);
+		bungeeCordLog.addHandler(mLogHandler);
+	}
+	
 }
