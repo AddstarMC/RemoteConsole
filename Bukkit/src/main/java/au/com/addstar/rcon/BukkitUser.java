b843821d0e90d0522ffef30440db959e3a9e1b57
Narimm <benjicharlton@gmail.com>
2017-06-10 22:47:34 +1000

    Update Spigot 1.12 and fix logging error

--- Bukkit/src/main/java/au/com/addstar/rcon/BukkitUser.java
+++ Bukkit/src/main/java/au/com/addstar/rcon/BukkitUser.java
@@ -1,8 +1,10 @@
 package au.com.addstar.rcon;
 
-import java.util.logging.Level;
 
+import com.google.common.collect.ImmutableMap;
 import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.Level;
+
 import org.bukkit.command.ConsoleCommandSender;
 
 import au.com.addstar.rcon.network.NetworkManager;
@@ -11,84 +13,90 @@ import au.com.addstar.rcon.network.packets.main.PacketOutMessage.MessageType;
 import au.com.addstar.rcon.server.User;
 import au.com.addstar.rcon.util.Message;
 
-public class BukkitUser extends User
-{
+public class BukkitUser extends User {
 	private UserCommandSender mSender;
-	
-	public BukkitUser(String name)
-	{
+
+	private static final ImmutableMap<Level, java.util.logging.Level> LEVELS =
+			new ImmutableMap.Builder<Level, java.util.logging.Level>()
+					.put(Level.ALL, java.util.logging.Level.ALL)
+					.put(Level.DEBUG, java.util.logging.Level.FINE)
+					.put(Level.ERROR, java.util.logging.Level.SEVERE)
+					.put(Level.FATAL, java.util.logging.Level.SEVERE)
+					.put(Level.INFO, java.util.logging.Level.INFO)
+					.put(Level.OFF, java.util.logging.Level.OFF)
+					.put(Level.TRACE, java.util.logging.Level.FINEST)
+					.put(Level.WARN, java.util.logging.Level.WARNING)
+					.build();
+
+	public BukkitUser(String name) {
 		super(name);
 	}
-	
-	public ConsoleCommandSender asCommandSender()
-	{
+
+	public ConsoleCommandSender asCommandSender() {
 		return mSender;
 	}
-	
+
 	@Override
-	public void setManager( NetworkManager manager )
-	{
+	public void setManager(NetworkManager manager) {
 		super.setManager(manager);
-		
-		if(manager == null)
-		{
+
+		if (manager == null) {
 			mSender.onRemove();
 			mSender = null;
-		}
-		else
+		} else
 			mSender = new UserCommandSender(this);
 	}
 
-	public void sendMessage( String message, MessageType type )
-	{
+	public void sendMessage(String message, MessageType type) {
 		mSender.sendMessage(message, type);
 	}
-	
-	public void sendLog( LogEvent event )
-	{
-		Level level;
-		switch(event.getLevel())
-		{
-		case ALL:
-			level = Level.ALL;
-			break;
-		case DEBUG:
-			level = Level.FINEST;
-			break;
-		case ERROR:
-			level = Level.SEVERE;
-			break;
-		case FATAL:
-			level = Level.SEVERE;
-			break;
-		case INFO:
-			level = Level.INFO;
-			break;
-		case OFF:
-			level = Level.OFF;
-			break;
-		case TRACE:
-			level = Level.FINER;
-			break;
-		case WARN:
-			level = Level.WARNING;
-			break;
-		default:
-			level = Level.FINE;
-			break;
-		}
-		
+
+	public void sendLog(LogEvent event) {
+		java.util.logging.Level level = LEVELS.get(event.getLevel());
+		if (level == null) level = java.util.logging.Level.FINE;
+//		switch(event.getLevel())
+//		{
+//		case ALL:
+//			level = Level.ALL;
+//			break;
+//		case DEBUG:
+//			level = Level.FINEST;
+//			break;
+//		case ERROR:
+//			level = Level.SEVERE;
+//			break;
+//		case FATAL:
+//			level = Level.SEVERE;
+//			break;
+//		case INFO:
+//			level = Level.INFO;
+//			break;
+//		case OFF:
+//			level = Level.OFF;
+//			break;
+//		case TRACE:
+//			level = Level.FINER;
+//			break;
+//		case WARN:
+//			level = Level.WARNING;
+//			break;
+//		default:
+//			level = Level.FINE;
+//			break;
+//		}
+
 		MessageType type = MessageType.Log;
-		if(event.getThrown() != null)
+		if (event.getThrown() != null)
 			type = MessageType.Exception;
-		else if(event.getThreadName().startsWith("Async Chat Thread"))
+		else if (event.getThreadName().startsWith("Async Chat Thread"))
 			type = MessageType.Chat;
-		
-		long time = event.getMillis();
+
+		long time = event.getTimeMillis();
 		String thread = event.getThreadName();
 		String message = event.getMessage().getFormattedMessage();
-		
+
 		PacketOutMessage packet = new PacketOutMessage(new Message(message, type, time, level, thread, event.getLoggerName()));
 		getManager().sendPacket(packet);
 	}
+
 }
